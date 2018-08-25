d3dec389784c2459a13c0a90fc7401bea6854fae
lihaojin <hli002@citymail.cuny.edu>
2018-08-25 17:44:44 -0400

    First commit

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -2,14 +2,18 @@
 //  ViewController.swift
 //  Tipper
 //
-//  Created by Candice Zhong on 8/25/18.
+//  Created by Haojin Li on 8/25/18.
 //  Copyright © 2018 Haojin Li. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercent = [0.15, 0.18, 0.20, 0.25]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercent[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+    
+    
+    
+    
 }
 
